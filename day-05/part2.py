584fd3ab777a22e07c10d7f55827e05b2a146736
oskari aalto <wawe.mr+github@gmail.com>
2024-08-12 21:52:04 +0300

    Setup for range based solution

--- day-05/part2.py
+++ day-05/part2.py
@@ -1,73 +1,74 @@
 import os
 
+""" The values on the initial seeds: line come in pairs. Within each pair, the
+first value is the start of the range and the second value is the length of
+the range. 
 """
-seed_to_soil
-soil_to_fertilizer
-fertilizer_to_water
-water_to_light
-light_to_temperature
-temperature_to_humidity
-humidity_to_location
 
-destination <<< source --- range length
-soil <<< seed
-fertilizer <<< soil
 """
+Dev notes
+- 1st solution tried to create a list with all the seed numbers.
+    With large input numbers that lead to running out of memory
+- 2nd solution looped through each seed range by using for loop and range function
+It takes a long time to calculate the final solution
+- 3d solution. Need to optimize the brute force solution. Reddit thread suggested
+    finding lowest value in each range.
+    Why this could work? As we are looking for the lowest location value it
+    would make sense that the smallest value in a range that is mapped also
+    produces the lowest location number. Of course there are multiple conversions
+    and a range might have to be split.
+
+    https://www.reddit.com/r/adventofcode/comments/18buwiz/comment/kc6mtif/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    "So when you map the ranges, you end up with a list of ranges that contain
+    all the results that you would have brute forced to check individually,
+    so you just pick the range with the lowest starting positon, and then pick
+    the first value in that range."
+ """
 
 def main():
-    input_file = 'input.txt'
+    input_file = 'test1.txt'
     data_folder = 'data'
     main_dir = os.path.split(os.path.abspath(__file__))[0]
     input_path = os.path.join(main_dir, data_folder, input_file)
 
-    seeds: list = []
+    seed_ranges: list = []
     map_ranges: list = []
-    key_map: int = -1
+    temp_ranges: list = []
 
     with open(input_path, 'r') as file:
         for line in file:
             if 'seeds:' in line:
                 seeds = [int(seed) for seed in line[6:].split()]
-                print(f'{seeds} : initial seeds')
-
-            if line == '\n':
-                if map_ranges:
-                    mapper(seeds, map_ranges)
-                    print(f'{seeds} : {key_map}')
-                    map_ranges.clear()
-                key_map += 1
-
-            elif key_map == 0 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 1 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 2 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 3 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 4 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 5 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 6 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
-            elif key_map == 7 and line[0].isdigit():
-                map_ranges.append(string_list_to_int_list(line.split()))
+                for i, range_start in enumerate(seeds):
+                    if i % 2 == 0:
+                        seed_ranges.append([range_start, seeds[i + 1]])
+                seeds.clear()
+            if line == '\n' and temp_ranges:
+                map_ranges.append(temp_ranges[:]) # copy list by value not ref
+                temp_ranges.clear()
+            elif line[0].isdigit():
+                temp_ranges.append(string_list_to_int_list(line.split()))
+    map_ranges.append(temp_ranges[:]) # copy list by value not ref
+    temp_ranges.clear()
     
-    mapper(seeds, map_ranges)
-    print(f'{seeds} : {key_map}')
-    print(f'Lowest location number: {min(seeds)}')
+    print(f'Lowest location number: {mapper(seed_ranges, map_ranges)}')
+            
+def mapper(seed_ranges: list, map_ranges: list) -> int:
+    lowest_location = -1
 
-            
-def mapper(seeds: list, map_ranges: list):
-    for pos, key in enumerate(seeds):
-        for row in map_ranges:
-            if key in range(row[1], row[1] + row[2]):
-                seeds[pos] = key + (row[0] - row[1])
-                break
-        else:
-            seeds[pos] = key
-            
+    for seed_range in seed_ranges:
+        for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
+            for conversion in map_ranges:   
+                for row in conversion:
+                    if seed in range(row[1], row[1] + row[2]):
+                        seed = seed + (row[0] - row[1])
+                        break
+            print(seed)
+    #         if seed < lowest_location or lowest_location == -1:
+    #             lowest_location = seed
+
+    # return lowest_location
+
 def string_list_to_int_list(data: list) -> list[int]:
     return [int(value) for value in data]
 
